c58bdb9b89b8d75a8f80c8c91581c5f5800c2499
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-16 03:06:31 +0500

    Task2

---
+++ Tyuiu.KozhevnikovYV.Sprint5.Task2.V17.Lib/DataService.cs
@@ -0,0 +1,52 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint5.Task2.V17.Lib
+{
+    using System.IO;
+    using System.Runtime.ExceptionServices;
+    using tyuiu.cources.programming.interfaces.Sprint5;
+    public class DataService : ISprint5Task2V17
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string tempDir = Path.GetTempPath();
+            string fileName = "OutPutFileTask2.csv";
+            string path = Path.Combine(tempDir, fileName);
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }    
+            }
+            string str = "";
+            for (int i = 0;i < rows; i++)
+            {
+                for (int j = 0;j < cols; j++)
+                {
+                    if (j != cols - 1)
+                    {
+                        str += matrix[i, j] + ";";
+                    }
+                    else
+                    {
+                        str += matrix[i, j];
+                    }
+                }
+                if (i != rows-1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+                str = "";
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovYV.Sprint5.Task2.V17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Tyuiu.KozhevnikovYV.Sprint5.Task2.V17.Lib;
+DataService ds = new DataService();
+int[,]matrix = new int[3, 3] { { 2, 1, 7 },
+                               { 1, 2, 4 },
+                               { 2, 3, 4 } };
+int rows = matrix.GetLength(0);
+int cols = matrix.GetLength(1);
+Console.Title = "Спринт 5 | Выполнил: Кожевников Я. В. | ПКТб-25-1";
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("* Спринт #5                                                                    *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                             *");
+Console.WriteLine("* Задание #2                                                                   *");
+Console.WriteLine("* Вариант #17                                                                  *");
+Console.WriteLine("* Выполнил: Кожевников Ярослав Вадимович | ПКТб-25-1                           *");
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("Массив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("********************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+Console.WriteLine("********************************************************************************");
+string res = ds.SaveToFileTextData(matrix);
+Console.WriteLine("Файл " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
