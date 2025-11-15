3b94ca6a449840ca78b56caef3df7c80e07ef460
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-15 23:02:50 +0500

    DataService.cs

--- Tyuiu.KozhevnikovYV.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.KozhevnikovYV.Sprint5.Task0.V7.Lib/DataService.cs
@@ -5,7 +5,9 @@ public class DataService : ISprint5Task0V7
 {
     public string SaveToFileTextData(int x)
     {
-        string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+        string tempDir = Path.GetTempPath();
+        string fileName = "OutPutFileTask0.txt";
+        string path = Path.Combine(tempDir, fileName);
         double z = -(Math.Pow(x, 3)) + 4 * Math.Pow(x, 2) - 1.5 * x;
         double res = Math.Round(z, 3);
         File.WriteAllText(path,Convert.ToString(res));
