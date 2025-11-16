6dee4cb09b1f86f30d2ff7922e0aba1f68b1e07f
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-16 05:39:41 +0500

    Task4

---
+++ Tyuiu.KozhevnikovYV.Sprint5.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint5.Task4.V3.Lib
+{
+    using System.Globalization;
+    using System.IO;
+    using tyuiu.cources.programming.interfaces.Sprint5;
+
+    public class DataService : ISprint5Task4V3
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            if (!double.TryParse(strX, NumberStyles.Any, CultureInfo.InvariantCulture, out double x))
+            {
+                Console.WriteLine("нипалучилос");
+            }
+            double res = Math.Pow(x, 3) * Math.Sin(x) - 4 * x;
+            res = Math.Round(res, 3);
+            return res;
+
+        }
+    }
+}
