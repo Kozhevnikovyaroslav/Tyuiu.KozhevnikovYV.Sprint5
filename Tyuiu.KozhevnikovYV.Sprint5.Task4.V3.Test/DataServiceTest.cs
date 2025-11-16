11d5624d8fdbede0b22c8c03ae3a62f2f9e65fe3
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-16 05:03:41 +0500

    Task4

---
+++ Tyuiu.KozhevnikovYV.Sprint5.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint5.Task4.V3.Test
+{u
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
