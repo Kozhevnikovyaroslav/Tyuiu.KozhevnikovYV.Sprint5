c202fe60ea04e7ec6e5fc17740a9fc3a5e29379d
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-24 01:10:53 +0500

    DataService.cs

--- Tyuiu.KozhevnikovYV.Sprint5.Task7.V12.Lib/DataService.cs
+++ Tyuiu.KozhevnikovYV.Sprint5.Task7.V12.Lib/DataService.cs
@@ -1,40 +1,39 @@
-﻿namespace Tyuiu.KozhevnikovYV.Sprint5.Task7.V12.Lib
+using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KozhevnikovYV.Sprint5.Task7.V12.Lib
 {
-    using tyuiu.cources.programming.interfaces.Sprint5;
     public class DataService : ISprint5Task7V12
     {
         public string LoadDataAndSave(string path)
         {
-            string DirPath = @"C:\DataSprint5";
-            string filename = "OutPutDataFileTask7V12.txt";
-            string pathSaveFile = Path.Combine(DirPath, filename);
-            FileInfo fileinfo = new FileInfo(pathSaveFile);
-            bool fileExists = fileinfo.Exists;
-            if (fileExists)
+            // 1. читаем весь текст из входного файла
+            string text = File.ReadAllText(path, Encoding.UTF8);
+
+            // 2. заменяем строчные русские буквы на заглавные
+            var sb = new StringBuilder(text.Length);
+
+            foreach (char c in text)
             {
-                File.Delete(pathSaveFile);
+                // диапазон 'а'..'я' + буква 'ё'
+                if ((c >= 'а' && c <= 'я') || c == 'ё')
+                    sb.Append(char.ToUpper(c));       // в верхний регистр
+                else
+                    sb.Append(c);                     // остальные символы без изменений
             }
-            string strLine = "";
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    foreach (char c in line)
-                    {
-                        if (!char.IsUpper(c))
-                        {
-                            strLine += char.ToUpper(c);
-                        }
-                        else
-                        {
-                            strLine += c;
-                        }
-                    }
-                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
-                }
-            }
-            return pathSaveFile;
+
+            string resultText = sb.ToString();
+
+            // 3. составляем путь к выходному файлу в temp-папке
+            string tempDir = Path.GetTempPath();                 // системная временная директория
+            string outFileName = "OutPutDataFileTask7V12.txt";   // имя по условию
+            string outPath = Path.Combine(tempDir, outFileName); // правильная склейка пути
+
+            // 4. записываем результат и возвращаем путь
+            File.WriteAllText(outPath, resultText, Encoding.UTF8);
+            return outPath;
         }
     }
 }
+
